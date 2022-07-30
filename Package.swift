32ac3802532b2a3bdac7f3d9c6e8d053037de3b5
Noe Jupiter <noeonjupiter@icloud.com>
2022-07-30 12:02:29 +0300

    Swift Tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["SImages"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/NoeOnJupiter/STools", "1.0.0"..<"2.0.0")
+        .package(url: "https://github.com/NoeOnJupiter/STools", "1.0.2"..<"1.0.5")
     ],
     targets: [
         .target(
