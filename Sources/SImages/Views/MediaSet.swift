30673ccceccffd4fae21f404d0e002b88454c831
Noe Jupiter <noeonjupiter@icloud.com>
2022-08-06 10:51:45 +0300

    Fixes

--- Sources/SImages/Views/MediaSet.swift
+++ Sources/SImages/Views/MediaSet.swift
@@ -22,7 +22,7 @@ public struct MediaSet<Medias: Mediabley, Content: View>: View {
     private let behavior: PhotosPickerSelectionBehavior
     private let library: PHPhotoLibrary
     private var contentForItem: ((Medias, Int) -> Content)?
-    private var contentForMedia: ((MediaImage<Text, Medias>, Medias, Int) -> Content)?
+    private var contentForMedia: ((DownsampledImage<Text>, Medias, Int) -> Content)?
 ///MediaSet: A MediaSet is a collection of Photos picked by the user. You can place it inside an HStack, VStack, LazyVGrid, LazyHGrid...
     public init(pickerPresented: Binding<Bool>, content: Binding<[Medias]>) {
         self._isPresented = pickerPresented
@@ -34,22 +34,36 @@ public struct MediaSet<Medias: Mediabley, Content: View>: View {
         self.library = PHPhotoLibrary.shared()
     }
     public var body: some View {
-        VStack {
-            ForEach(Array(content.enumerated()), id: \.offset) { index, item in
-                if let contentForItem {
-                    contentForItem(item, index)
-                }else if let contentForMedia {
-                    contentForMedia(MediaImage<Text, Medias>(mediable: .binding($content[index])).disabledPicker(), item, index)
-                }
+        Group {
+            ForEach(Array(content.enumerated()), id: \.element) { index, item in
+                contentForMedia?(DownsampledImage<Text>(image: .binding($content[index].data.unImage)), item, index)
             }
         }.photosPicker(isPresented: $isPresented, selection: $pickerItems, maxSelectionCount: maxSelectionCount, selectionBehavior: behavior, matching: filter, preferredItemEncoding: encoding, photoLibrary: library)
+        .onChange(of: pickerItems) { newValue in
+            newValue.forEach { item in
+                updateState(pickerItem: item)
+            }
+        }
     }
 }
 
 //MARK: - Private Initializer
 @available(iOS 16.0, *)
 private extension MediaSet {
-    init(isPresented: Binding<Bool>, content: Binding<[Medias]>, filter: PHPickerFilter?, encoding: PhotosPickerItem.EncodingDisambiguationPolicy, maxSelectionCount: Int?, behavior: PhotosPickerSelectionBehavior, library: PHPhotoLibrary, contentForItem: ((Medias, Int) -> Content)?, contentForMedia: ((MediaImage<Text, Medias>, Medias, Int) -> Content)?) {
+    func updateState(pickerItem: PhotosPickerItem?) {
+        guard let pickerItem = pickerItem else {return}
+        Task {
+            guard let image = try? await pickerItem.loadTransferable(type: Data.self) else {return}
+            DispatchQueue.main.async { [self] in
+                withAnimation() {
+                    var media = Medias.empty
+                    media.data = image
+                    content.append(media)
+                }
+            }
+        }
+    }
+    init(isPresented: Binding<Bool>, content: Binding<[Medias]>, filter: PHPickerFilter?, encoding: PhotosPickerItem.EncodingDisambiguationPolicy, maxSelectionCount: Int?, behavior: PhotosPickerSelectionBehavior, library: PHPhotoLibrary, contentForItem: ((Medias, Int) -> Content)?, contentForMedia: ((DownsampledImage<Text>, Medias, Int) -> Content)?) {
         self._isPresented = isPresented
         self._content = content
         self.filter = filter
@@ -90,7 +104,7 @@ public extension MediaSet {
         MediaSet(isPresented: $isPresented, content: $content, filter: filter, encoding: encoding, maxSelectionCount: maxSelectionCount, behavior: behavior, library: library, contentForItem: contentForItem, contentForMedia: contentForMedia)
     }
 ///MediaSet: Assing the MediaImage to be displayed for each of the picked Photos
-    func content(@ViewBuilder contentForMedia: @escaping (MediaImage<Text, Medias>, Medias, Int) -> Content) -> Self {
+    func content(@ViewBuilder contentForMedia: @escaping (DownsampledImage<Text>, Medias, Int) -> Content) -> Self {
         MediaSet(isPresented: $isPresented, content: $content, filter: filter, encoding: encoding, maxSelectionCount: maxSelectionCount, behavior: behavior, library: library, contentForItem: contentForItem, contentForMedia: contentForMedia)
     }
 }
