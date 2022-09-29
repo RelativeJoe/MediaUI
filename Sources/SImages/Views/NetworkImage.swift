dce16908fb3ef2f99f5efcddb90021e36ac5201c
Noe Jupiter <noeonjupiter@icloud.com>
2022-09-29 18:31:42 +0300

    NetworkImage

--- Sources/SImages/Views/NetworkImage.swift
+++ Sources/SImages/Views/NetworkImage.swift
@@ -22,30 +22,32 @@ public struct NetworkImage: View {
     private let aspectRatio: (CGFloat?, ContentMode)?
     private let loading: AnyView?
     public var body: some View {
-        ZStack {
-            Color.clear
-                .onTask(id: "NetworkImage") {
-                    guard unImage == nil else {return}
-                    imageState = .loading
-                    guard let imageURL else {
-                        error = "Invalid Image URL"
-                        imageState = .error
-                        return
-                    }
-                    if let image = ImageConfigurations.cache.image(for: imageURL) {
-                        unImage = image
+        Color.clear
+            .onTask {
+                guard unImage == nil else {return}
+                imageState = .loading
+                guard let imageURL else {
+                    error = "Invalid Image URL"
+                    imageState = .error
+                    return
+                }
+                if let image = ImageConfigurations.cache.image(for: imageURL) {
+                    unImage = image
+                    imageState = .idle
+                }else {
+                    do {
+                        let data = try await URLSession.shared.data(from: imageURL).0
+                        unImage = UNImage(data: data)
                         imageState = .idle
-                    }else {
-                        do {
-                            let data = try await URLSession.shared.data(from: imageURL).0
-                            unImage = UNImage(data: data)
-                            imageState = .idle
-                        }catch {
-                            self.error = error.localizedDescription
-                            imageState = .error
-                        }
+                    }catch {
+                        self.error = error.localizedDescription
+                        imageState = .error
                     }
                 }
+            }.overlay(image)
+    }
+    var image: some View {
+        Group {
             switch imageState {
                 case .idle:
                     if let unImage {
@@ -57,7 +59,7 @@ public struct NetworkImage: View {
                     if let loading {
                         loading
                     }else {
-                        //ActivityView()
+                        ActivityView()
                     }
                 case .error:
                     Text(error ?? "Unknown Error")
@@ -68,7 +70,7 @@ public struct NetworkImage: View {
 
 //MARK: - Public Initializers
 public extension NetworkImage {
-///SImages: Initialize a NetworkImage from a String.
+    ///SImages: Initialize a NetworkImage from a String.
     init(url: String?) {
         if let url {
             self.imageURL = URL(string: url)
@@ -81,7 +83,7 @@ public extension NetworkImage {
         self.placeHolder = nil
         self.loading = nil
     }
-///SImages: Initialize a NetworkImage from an URL.
+    ///SImages: Initialize a NetworkImage from an URL.
     init(url: URL?) {
         self.imageURL = url
         self.height = nil
@@ -113,27 +115,27 @@ internal extension NetworkImage {
 
 ///MARK: - Public Modifiers
 public extension NetworkImage {
-///NetworkImage: Make the Image take the Shape of a square.
+    ///NetworkImage: Make the Image take the Shape of a square.
     func squaredImage() -> Self {
         NetworkImage(imageState: imageState, error: error, unImage: unImage, imageURL: imageURL, height: height, width: width, placeHolder: placeHolder, squared: true, resizable: resizable, aspectRatio: aspectRatio, loading: loading)
     }
-///NetworkImage: Sets the mode by which SwiftUI resizes an Image to fit it's space.
+    ///NetworkImage: Sets the mode by which SwiftUI resizes an Image to fit it's space.
     func isResizable() -> Self {
         NetworkImage(imageState: imageState, error: error, unImage: unImage, imageURL: imageURL, height: height, width: width, placeHolder: placeHolder, squared: squared, resizable: true, aspectRatio: aspectRatio, loading: loading)
     }
-///NetworkImage: Constrains this View's dimesnions to the specified aspect rario.
+    ///NetworkImage: Constrains this View's dimesnions to the specified aspect rario.
     func aspect(_ ratio: CGFloat? = nil, contentMode: ContentMode) -> Self {
         NetworkImage(imageState: imageState, error: error, unImage: unImage, imageURL: imageURL, height: height, width: width, placeHolder: placeHolder, squared: squared, resizable: resizable, aspectRatio: (ratio, contentMode), loading: loading)
     }
-///NetworkImage: Positions this View within an invisible frame with the specified size.
+    ///NetworkImage: Positions this View within an invisible frame with the specified size.
     func frame(width: CGFloat? = nil, height: CGFloat? = nil) -> Self  {
         NetworkImage(imageState: imageState, error: error, unImage: unImage, imageURL: imageURL, height: height, width: width, placeHolder: placeHolder, squared: squared, resizable: resizable, aspectRatio: aspectRatio, loading: loading)
     }
-///NetworkImage: Adds a placeholder View if no Image can be displayed.
+    ///NetworkImage: Adds a placeholder View if no Image can be displayed.
     func placeHolder(@ViewBuilder placeholder: () -> some View) -> Self {
         NetworkImage(imageState: imageState, error: error, unImage: unImage, imageURL: imageURL, height: height, width: width, placeHolder: AnyView(placeholder()), squared: squared, resizable: resizable, aspectRatio: aspectRatio, loading: loading)
     }
-///NetworkImage: Customize the loading View.
+    ///NetworkImage: Customize the loading View.
     func onLoading(@ViewBuilder loading: () -> some View) -> Self {
         NetworkImage(imageState: imageState, error: error, unImage: unImage, imageURL: imageURL, height: height, width: width, placeHolder: placeHolder, squared: squared, resizable: resizable, aspectRatio: aspectRatio, loading: AnyView(loading()))
     }
